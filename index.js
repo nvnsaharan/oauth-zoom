f92923ba087b936243d03ee5be3b7068cfa1a89a
nvnsaharan <bb1180026@iitd.ac.in>
2021-12-09 11:12:24 +0530

    dv

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get("/", (req, res) => {
         request
             .post(url, (error, response, body) => {
                 body = JSON.parse(body);
-                console.log("Caaling 2");
+                console.log(body.access_token);
                 if (body.access_token) {
                     request
                         .get(
